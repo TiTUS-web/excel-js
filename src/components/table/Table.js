2ae70a4f6aedb65e2021ac39e897764fdef3b5f4
diyanchuk <diynchuk@fmf.ru>
2022-02-24 17:47:22 +0400

    creation of interactive

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -16,7 +16,7 @@ export class Table extends ExcelComponent {
   constructor($root, options) {
     super($root, {
       name: 'Table',
-      listeners: ['mousedown', 'keydown'],
+      listeners: ['mousedown', 'keydown', 'input'],
       ...options,
     });
   }
@@ -33,11 +33,16 @@ export class Table extends ExcelComponent {
     super.init();
 
     const $cell = this.$root.find('[data-id="0:0"]');
-    this.selection.select($cell);
+    this.selectCell($cell);
 
-    this.emitter.subscribe('successEmit', (text) =>
-      this.selection.current.text(text)
-    );
+    this.$on('formula:input', (text) => this.selection.current.text(text));
+
+    this.$on('formula:done', () => this.selection.current.focus());
+  }
+
+  selectCell($cell) {
+    this.selection.select($cell);
+    this.$emit('table:select', $cell);
   }
 
   onMousedown(event) {
@@ -74,7 +79,11 @@ export class Table extends ExcelComponent {
       const id = this.selection.current.id(true);
 
       const $next = this.$root.find(nextSelector(key, id));
-      this.selection.select($next);
+      this.selectCell($next);
     }
+  }
+
+  onInput(event) {
+    this.$emit('table:input', $(event.target));
   }
 }
