efa2beee8fadb8bece5b228fe822d9674fd0b7c7
diyanchuk <diynchuk@fmf.ru>
2022-03-08 19:42:00 +0400

    change and save table name

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,6 +3,7 @@ import {
   CHANGE_TEXT,
   CHANGE_STYLES,
   APPLY_STYLE,
+  CHANGE_TITLE,
 } from '@/redux/types';
 
 export function rootReducer(state, action) {
@@ -32,6 +33,8 @@ export function rootReducer(state, action) {
         [field]: val,
         currentStyles: {...state.currentStyles, ...action.data.value},
       };
+    case CHANGE_TITLE:
+      return {...state, title: action.data};
     default:
       return state;
   }
